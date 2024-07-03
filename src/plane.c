5df0b21f36d6cbbf0232d069ae8c7652f7407660
Hiroaki Ono <hiono@student.42adel.org.au>
2024-07-03 18:14:41 +0930

    add plane and fixed norm

--- src/plane.c
+++ src/plane.c
@@ -1,30 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sphere.c                                           :+:      :+:    :+:   */
+/*   plane.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hiono <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/07/02 18:44:00 by hiono             #+#    #+#             */
-/*   Updated: 2024/07/03 17:27:30 by hiono            ###   ########.fr       */
+/*   Created: 2024/07/03 17:10:29 by hiono             #+#    #+#             */
+/*   Updated: 2024/07/03 17:26:48 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/miniRT.h"
 
-// if no intersection, return value is -1
-double	to_sphere_intersection(t_sphere sphere, t_vector origin, t_vector line)
+// if no intersection or line inside plane, return value is -1
+double	to_plane_intersection(t_plane plane, t_vector origin, t_vector line)
 {
-	double	grad;
 	double	distance;
 
 	line = normalise(line);
-	grad = pow(dot(line, subtract(origin, sphere.position)), 2.0)
-		- pow(magnitude(subtract(origin, sphere.position)), 2.0)
-		+ pow((sphere.diameter / 2.0), 2.0);
-	if (grad < 0)
+	if (dot(line, plane.normal) == 0)
 		return (-1);
-	distance = -dot(line, subtract(origin, sphere.position)) - sqrt(grad);
+	distance = dot(subtract(plane.position, origin), plane.normal)
+		/ dot(line, plane.normal);
 	if (distance < 0)
 		return (-1);
 	return (distance);
