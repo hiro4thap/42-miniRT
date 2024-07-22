e62feaf2232edcc054df06d3369acbc82bb979b1
Hiroaki Ono <hiono@student.42adel.org.au>
2024-07-22 15:25:00 +0930

    fixed norms

--- bonus/src/validate_objects.c
+++ bonus/src/validate_objects.c
@@ -6,38 +6,55 @@
 /*   By: jhughes <jhughes@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 15:22:03 by hiono             #+#    #+#             */
-/*   Updated: 2024/07/22 13:59:16 by hiono            ###   ########.fr       */
+/*   Updated: 2024/07/22 15:23:59 by hiono            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/miniRT.h"
 
+static int	validate_flags(const char *line, char *object_type)
+{
+	int	index;
+
+	index = 0;
+	if (!validate_flag(&line[index]))
+	{
+		ft_putendl_fd("Error", STDERR_FILENO);
+		ft_putstr_fd("Invalid flag for ", STDERR_FILENO);
+		ft_putstr_fd(object_type, STDERR_FILENO);
+		ft_putendl_fd(" material", STDERR_FILENO);
+		return (0);
+	}
+	index += validate_flag(&line[index]);
+	if (!validate_flag(&line[index]))
+	{
+		ft_putendl_fd("Error", STDERR_FILENO);
+		ft_putstr_fd("Invalid flag for ", STDERR_FILENO);
+		ft_putstr_fd(object_type, STDERR_FILENO);
+		ft_putendl_fd(" bump map", STDERR_FILENO);
+		return (0);
+	}
+	index += validate_flag(&line[index]);
+	return (index);
+}
+
 t_bool	is_valid_sphere(const char *line)
 {
 	int	index;
-	int	result;
 
 	index = ft_strlen("sp");
-	result = validate_coordinate(&line[index]);
-	if (!result)
+	if (!validate_coordinate(&line[index]))
 		return (err("Invalid coordinate for sphere center"));
-	index += result;
-	result = validate_udouble(&line[index]);
-	if (!result)
+	index += validate_coordinate(&line[index]);
+	if (!validate_udouble(&line[index]))
 		return (err("Invalid double for sphere diameter"));
-	index += result;
-	result = validate_rgb(&line[index]);
-	if (!result)
+	index += validate_udouble(&line[index]);
+	if (!validate_rgb(&line[index]))
 		return (err("Invalid rgb for sphere color"));
-	index += result;
-	result = validate_flag(&line[index]);
-	if (!result)
-		return (err("Invalid flag for sphere material"));
-	index += result;
-	result = validate_flag(&line[index]);
-	if (!result)
-		return (err("Invalid flag for sphere bump map"));
-	index += result;
+	index += validate_rgb(&line[index]);
+	if (!validate_flags(&line[index], "sphere"))
+		return (0);
+	index += validate_flags(&line[index], "sphere");
 	if (line[index] != '\n' && line[index] != '\0')
 		return (err("Excessive information for sphere"));
 	return (TRUE);
@@ -46,29 +63,20 @@ t_bool	is_valid_sphere(const char *line)
 t_bool	is_valid_plane(const char *line)
 {
 	int	index;
-	int	result;
 
 	index = ft_strlen("pl");
-	result = validate_coordinate(&line[index]);
-	if (!result)
+	if (!validate_coordinate(&line[index]))
 		return (err("Invalid coordinate for plane point"));
-	index += result;
-	result = validate_normalized_vector(&line[index]);
-	if (!result)
+	index += validate_coordinate(&line[index]);
+	if (!validate_normalized_vector(&line[index]))
 		return (err("Invalid normalized vector for plane normal vector"));
-	index += result;
-	result = validate_rgb(&line[index]);
-	if (!result)
+	index += validate_normalized_vector(&line[index]);
+	if (!validate_rgb(&line[index]))
 		return (err("Invalid rgb for plane color"));
-	index += result;
-	result = validate_flag(&line[index]);
-	if (!result)
-		return (err("Invalid flag for plane material"));
-	index += result;
-	result = validate_flag(&line[index]);
-	if (!result)
-		return (err("Invalid flag for plane bump map"));
-	index += result;
+	index += validate_rgb(&line[index]);
+	if (!validate_flags(&line[index], "plane"))
+		return (0);
+	index += validate_flags(&line[index], "plane");
 	if (line[index] != '\n' && line[index] != '\0')
 		return (err("Excessive information for plane"));
 	return (TRUE);
@@ -94,12 +102,9 @@ t_bool	is_valid_cylinder(const char *line)
 	if (!validate_rgb(&line[index]))
 		return (err("Invalid rgb for cylinder color"));
 	index += validate_rgb(&line[index]);
-	if (!validate_flag(&line[index]))
-		return (err("Invalid flag for cylinder material"));
-	index += validate_flag(&line[index]);
-	if (!validate_flag(&line[index]))
-		return (err("Invalid flag for cylinder bump map"));
-	index += validate_flag(&line[index]);
+	if (!validate_flags(&line[index], "cylinder"))
+		return (0);
+	index += validate_flags(&line[index], "cylinder");
 	if (line[index] != '\n' && line[index] != '\0')
 		return (err("Excessive information for cylinder"));
 	return (TRUE);
@@ -125,12 +130,9 @@ t_bool	is_valid_cone(const char *line)
 	if (!validate_coordinate(&line[index]))
 		return (err("Invalid rgb for cone color"));
 	index += validate_coordinate(&line[index]);
-	if (!validate_flag(&line[index]))
-		return (err("Invalid flag for cone material"));
-	index += validate_flag(&line[index]);
-	if (!validate_flag(&line[index]))
-		return (err("Invalid flag for cone bump map"));
-	index += validate_flag(&line[index]);
+	if (!validate_flags(&line[index], "cone"))
+		return (0);
+	index += validate_flags(&line[index], "cone");
 	if (line[index] != '\n' && line[index] != '\0')
 		return (err("Excessive information for cone"));
 	return (TRUE);
